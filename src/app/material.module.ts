366492377e49d16875c1ed88718b7f6fe991d922
Ricardo <ricardo@ricardofideles.com.br>
2020-11-03 18:29:38 -0300

    Adding a datePicker component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   imports: [
@@ -11,12 +14,19 @@ import {MatInputModule} from '@angular/material/input';
     MatIconModule, 
     MatDividerModule, 
     MatFormFieldModule, 
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     MatInputModule],
   exports: [
     MatButtonModule, 
     MatIconModule, 
     MatDividerModule, 
     MatFormFieldModule, 
-    MatInputModule]
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }]
 })
 export class MaterialModule {}
