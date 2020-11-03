28a673c49b3b96a7973f2f63434a59a01acbaa72
Ricardo <ricardo@ricardofideles.com.br>
2020-11-03 17:31:32 -0300

    Loading more angular-materials components.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatDividerModule],
-  exports: [MatButtonModule, MatIconModule, MatDividerModule]
+  imports: [
+    MatButtonModule, 
+    MatIconModule, 
+    MatDividerModule, 
+    MatFormFieldModule, 
+    MatInputModule],
+  exports: [
+    MatButtonModule, 
+    MatIconModule, 
+    MatDividerModule, 
+    MatFormFieldModule, 
+    MatInputModule]
 })
 export class MaterialModule {}
