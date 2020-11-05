5ba07800dead2d80e7962c9fd74ea776ac763543
Ricardo Jorge Fideles Junior <ricardo@MacBook-Pro-de-Ricardo.local>
2020-11-05 10:49:05 -0300

    Creating the training area for the app.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -11,6 +11,10 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSelectModule} from '@angular/material/select';
+
+
 
 
 @NgModule({
@@ -26,6 +30,8 @@ import {MatListModule} from '@angular/material/list';
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
+    MatTabsModule,
+    MatSelectModule,
     MatInputModule],
   exports: [
     MatButtonModule, 
@@ -39,6 +45,8 @@ import {MatListModule} from '@angular/material/list';
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
+    MatTabsModule,
+    MatSelectModule,
     MatInputModule],
   providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }]
 })
